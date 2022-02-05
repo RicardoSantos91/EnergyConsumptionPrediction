aab5d47b542e9d8fec179351c56c6ac67f24b750
Ricardo M. Santos <Ricardomanuelsantos91@gmail.com>
2022-02-05 16:20:39 +0100

    Remove NAs

--- ImporDataAndTreatData.py
+++ ImporDataAndTreatData.py
@@ -1,3 +1,6 @@
+#### Module to import the data and treat for duplicates and NAs ####
+#### Author: Ricardo Santos ####
+
 import pandas as pd
 import os
 
@@ -5,6 +8,7 @@ cwd = os.getcwd()
 
 building_data = pd.read_feather('site0.ft')
 
+# Describe the dataset
 
 def data_characterization(df):
 
@@ -16,6 +20,7 @@ def data_characterization(df):
 
 data_info, data_description = data_characterization(building_data)
 
+# Identify duplicates
 
 def data_duplicates(df):
 
@@ -29,6 +34,39 @@ def data_duplicates(df):
 
 
 data_duplicates(building_data)
+
+# Identify NAs
+
+def data_nas(df):
+
+    nas = sum(building_data.isna().sum())
+    if nas == 0:
+        print('There are no NAs')
+    else:
+        print('There are ' + str(nas) + ' duplicated entries')
+
+    return 0
+
+
+data_nas(building_data)
+
+# Remove Nas
+
+
+def drop_nas(df):
+
+    df = df.dropna()
+
+    return df
+
+
+building_data = drop_nas(building_data)
+
+
+# Save dataset to ft
+
+
+
 
 
 
---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# Time series prediction on UCF buildings dataset
+
+Original dataset:
+
+link: https://www.kaggle.com/teeyee314/ucf-building-meter-reading
+
+Goals:
+
+1. Treat the data
+2. Visualize the data
+3. Predict next year of building consumption by meter type (Electricity and chiller)
